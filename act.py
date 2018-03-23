5389ce623f431f743b88959e27c709bef9440b82
unt <tw1701@nyu.edu>
2018-03-23 19:39:07 -0400

    add argv[2] and argv[3]

--- act.py
+++ act.py
@@ -1,12 +1,24 @@
 import sys
 from list import *
 
+argc = len(sys.argv)
+if argc > 3:
+    trans_to = sys.argv[3]
+else:
+    trans_to = 'poly' # 'ipa_u'
+if argc > 2:
+    trans_from = sys.argv[2]
+else:
+    trans_from = 'zimu'
 input_str = sys.argv[1]
 
 word = input_str.split()
 for i, s in enumerate(word):
-    consonant_no = consonant['zimu'].index(s)
-    word[i] = consonant['poly'][consonant_no]
+    if s not in consonant[trans_from] and trans_from == 'zimu':
+        consonant_no = consonant['vari'].index(s)
+    else:
+        consonant_no = consonant[trans_from].index(s)
+    word[i] = consonant[trans_to][consonant_no]
 
 output_str = ' '.join(word)
 
